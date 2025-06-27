876c8415fc9c6a110b743af900040d398bcd15b4
Faraz Shaikh <farazzshaikh@gmail.com>
2025-06-27 16:17:09 +0400

    Update submodule demo-2023-r3f-webgpu

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./main.css";
-import { UI } from "./UI";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <UI />
   </React.StrictMode>
 );
